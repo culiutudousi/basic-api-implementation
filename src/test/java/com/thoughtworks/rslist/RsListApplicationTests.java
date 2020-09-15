f124ff056fb64ab295ab42fc4186adae8d7c0b39
culiutudousi <chenzhicheng1994@gmail.com>
2020-09-15 07:35:57 -0700

    can add event to server

--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,15 +1,19 @@
 package com.thoughtworks.rslist;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -62,6 +66,24 @@ class RsListApplicationTests {
                 .andExpect(jsonPath("$[1].keyWord", is("no tag")))
                 .andExpect(jsonPath("$[2].eventName", is("3rd event")))
                 .andExpect(jsonPath("$[2].keyWord", is("no tag")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void should_add_re_event() throws Exception {
+        String jsonString = "{\"eventName\":\"pork rise in price\",\"keyWord\":\"economic\"}";
+        mockMvc.perform(post("/rs/event").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(jsonPath("$", hasSize(4)))
+                .andExpect(jsonPath("$[0].eventName", is("1st event")))
+                .andExpect(jsonPath("$[0].keyWord", is("no tag")))
+                .andExpect(jsonPath("$[1].eventName", is("2ed event")))
+                .andExpect(jsonPath("$[1].keyWord", is("no tag")))
+                .andExpect(jsonPath("$[2].eventName", is("3rd event")))
+                .andExpect(jsonPath("$[2].keyWord", is("no tag")))
+                .andExpect(jsonPath("$[3].eventName", is("pork rise in price")))
+                .andExpect(jsonPath("$[3].keyWord", is("economic")))
                 .andExpect(status().isOk());
     }
 }
