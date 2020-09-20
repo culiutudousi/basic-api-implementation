74c69159e8b88544bf193edab512b410b1aa2e72
culiutudousi <chenzhicheng1994@gmail.com>
2020-09-20 23:56:57 +0800

    [Day 05] bean, replace @Service by @Bean

--- src/main/java/com/thoughtworks/rslist/service/RsEventService.java
+++ src/main/java/com/thoughtworks/rslist/service/RsEventService.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@Service
+//@Service
 public class RsEventService {
     final RsEventRepository rsEventRepository;
     final UserRepository userRepository;
---
+++ src/main/java/com/thoughtworks/rslist/service/RsEventServiceConfiguration.java
@@ -0,0 +1,23 @@
+package com.thoughtworks.rslist.service;
+
+import com.thoughtworks.rslist.repository.RsEventRepository;
+import com.thoughtworks.rslist.repository.UserRepository;
+import com.thoughtworks.rslist.repository.VoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RsEventServiceConfiguration {
+    @Autowired
+    RsEventRepository rsEventRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    VoteRepository voteRepository;
+
+    @Bean
+    public RsEventService getRsEventService() {
+        return new RsEventService(rsEventRepository, userRepository, voteRepository);
+    }
+}
